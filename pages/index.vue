56b1c5d67a1410fbd90363f80592d21f7df2c446
Rooter125 <ramenpro55@gmail.com>
2025-03-20 20:16:39 +0300

    Second
    Second

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
 <template>
-    <p class="text-2xl">Hello</p>
+       <div class="flex justify-around">
+           <p class="text-2xl">Hello</p>
+           <p class="text-2xl">Go</p>
+           <p class="text-2xl">Goodbye</p>
+       </div>
 </template>
