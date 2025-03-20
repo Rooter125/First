2a278ca345b77661c95afdc257caa7363a462e68
Rooter125 <ramenpro55@gmail.com>
2025-03-20 20:22:19 +0300

    Fifth
    Fifth

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
        <div class="flex justify-around">
            <p class="text-2xl">Hello</p>
            <p class="text-2xl">Go</p>
-           <p class="text-2xl">Nexts</p>
+           <p class="text-2xl">Next</p>
            <p class="text-2xl">Goodbye</p>
        </div>
 </template>
